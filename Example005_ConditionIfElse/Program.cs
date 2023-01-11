3e858b4b87bd926615f0cb02562e2e751dd7c799
«Nodzomy» <>
2023-01-11 10:17:49 +0300

    Add Example010

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Write("Введите имя пользователя:  ");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "миша")
+if (username.ToLower() == "миша")
 {
     Console.WriteLine("Ура, это же Миша!");
 }
 else
 {
-  Console.Write("Првиет, ");
-  Console.WriteLine(username);  
+    Console.Write("Првиет, ");
+    Console.WriteLine(username);
 }
