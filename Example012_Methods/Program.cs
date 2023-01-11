697542eb3574f50851b6daaa06de6faf5c7d16af
«Nodzomy» <>
2023-01-11 13:56:46 +0300

    Add Example012 + TaskArray

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -60,18 +60,98 @@ int year = Method3();
 
 
 // Вид 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
-string res = Method4(10, "asdf");
-Console.WriteLine(res);
+string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, -что,"
+            + "ежели бы вас послали всемсто нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             012345
+// s[3] // к
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";        
+//     }
+
+//     return result;
+// }
+
+// string NewText = Replace(text, ' ', '|' );
+// Console.WriteLine(NewText);
+// Console.WriteLine();
+
+// NewText = Replace(NewText, 'к', 'К' );
+// Console.WriteLine(NewText);
+// Console.WriteLine();
+
+// NewText = Replace(NewText, 'С', 'с' );
+// Console.WriteLine(NewText);
+// Console.WriteLine();
+
+
+int[] arr =={1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arr);
